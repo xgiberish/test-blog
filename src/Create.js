d52875b3981c24676b9b72b7f0a9dfe9bd2d3ca6
Dina Fallah <dina.a.fallah@gmail.com>
2023-08-09 14:36:30 +0300

    Added new blog page

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,22 @@
 const Create = () => {
     return ( <div className="create">
         <h2>Create a new Blog</h2>
+        <form>
+            <label>Blog title:</label>
+            <input
+            type="text"
+            required
+            />
+            <label>Blog body:</label>
+            <textarea required>
+            </textarea>
+            <label>Blog author:</label>
+            <select>
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+            </select>
+            <button>Add Blog</button>
+        </form>
     </div> );
 }
  
